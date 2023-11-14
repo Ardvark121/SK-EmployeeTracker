b7e1803af444a2f33dabf0dc906ca6a25cd41d4d
Shawn Kim <kimshawnj@gmail.com>
2023-11-14 14:08:19 -0600

    did a full rewrite on all code espexially index js

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (id, name)
+  VALUES (6, "Geobank");
+
+INSERT INTO role (id, title, salary, department_id)
+  VALUES (2, "Manager", "23423423", "6");
+
+INSERT INTO employee (id, first_name, last_name, role_id)
+  VALUES (1, "John", "Man", "2");
--- index.js
+++ index.js
@@ -2,63 +2,80 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 // formats response from db
 const ctable = require("console.table");
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    user: "root",
-    password: "MyN3wP4ssw0rd",
-    database: "CMS_db",
-  },
-  console.log(`Connected to the books_db database.`)
-);
-inquirer
-  .prompt([
+const db = mysql
+  .createConnection(
     {
-      type: "list",
-      name: "Selected",
-      message: "What would you like to do?",
-      choices: [
-        "View All Employees",
-        "Add Employee",
-        "Update Employee Role",
-        "View All Roles",
-        "Add Role",
-        "View All Departments",
-        "Add Departments",
-      ],
+      host: "localhost",
+      user: "root",
+      password: "MyN3wP4ssw0rd",
+      database: "CMS_db",
     },
-  ])
-  .then((answer) => {
-    const SelectedOption = answer.Selected;
-    if (SelectedOption == "View All Employees") {
-      console.log(db.employee);
-    } else if (SelectedOption == "View All Departments") {
-      getdepartment();
-    } else if (SelectedOption == "View All Roles") {
-      console.log(db.role);
-    } else if (SelectedOption == "Add Employee") {
-      console.log("not yet");
-    } else if (SelectedOption == "Update Employee Role") {
-      console.log("not yet");
-    } else if (SelectedOption == "Add Role") {
-      console.log("not yet");
-    } else if (SelectedOption == "Add Departments") {
-      addDepartment();
-    }
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log("Prompt couldn't be rendered in the current environment");
-    } else {
-      console.error(error);
-    }
-  });
+    console.log(`Connected to the cms_db database.`)
+  )
+  .promise();
+function startpage() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "Selected",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Departments",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const SelectedOption = answer.Selected;
+      if (SelectedOption == "View All Employees") {
+        getemployee();
+      } else if (SelectedOption == "View All Departments") {
+        getdepartment();
+      } else if (SelectedOption == "View All Roles") {
+        getrole();
+      } else if (SelectedOption == "Add Employee") {
+        addEmployee();
+      } else if (SelectedOption == "Update Employee Role") {
+        console.log("not yet");
+      } else if (SelectedOption == "Add Role") {
+        addRole();
+      } else if (SelectedOption == "Add Departments") {
+        addDepartment();
+      }
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        console.log("Prompt couldn't be rendered in the current environment");
+      } else {
+        console.error(error);
+      }
+    });
+}
+async function getdepartment() {
+  const [results] = await db.query("SELECT name FROM department");
+  const departmentNames = results.map((row) => row.name);
+  console.log(departmentNames);
+  startpage();
+}
 
-function getdepartment() {
-  db.query("SELECT name FROM department", (err, res) => {
-    if (err) throw err;
-    console.table(res);
-  });
+async function getrole() {
+  const results = await db.query("SELECT title FROM role");
+  const roleNames = results.map((row) => row.name);
+  console.log(roleNames);
+  startpage();
+}
+
+async function getemployee() {
+  const results = await db.query("SELECT first_name FROM employee");
+  const employeeNames = results.map((row) => row.name);
+  console.log(employeeNames);
+  startpage();
 }
 
 function addDepartment() {
@@ -72,12 +89,90 @@ function addDepartment() {
     ])
     .then((input) => {
       db.query(
-        `INSERT INTO department SET name =? `,
+        "INSERT INTO department SET name =?",
         [input.departname],
         (err, res) => {
           if (err) throw err;
-          getdepartment();
         }
       );
+      startpage();
     });
 }
+
+async function addRole() {
+  const input = await inquirer.prompt([
+    {
+      type: "input",
+      name: "roleTitle",
+      message: "What is the title of the new role?",
+    },
+    {
+      type: "input",
+      name: "roleSalary",
+      message: "What is the salary of the new role?",
+    },
+    {
+      type: "input",
+      name: "departmentName",
+      message: "Enter the department name:",
+    },
+  ]);
+  const [departquery] = await db.query(
+    "SELECT id FROM department WHERE name = ?",
+    [input.departmentName]
+  );
+  department_id = departquery[0].id;
+
+  const [result] = await db.query(
+    "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)",
+    [input.roleTitle, input.roleSalary, department_id]
+  );
+
+  console.log("Role added successfully");
+  startpage();
+}
+
+async function addEmployee() {
+  const input = await inquirer.prompt([
+    {
+      type: "input",
+      name: "employeeFirstName",
+      message: "What is your firstname?",
+    },
+    {
+      type: "input",
+      name: "employeeLastName",
+      message: "What is your lastname?",
+    },
+    {
+      type: "input",
+      name: "roleName",
+      message: "Enter the title of your role",
+    },
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the first name of your manager",
+    },
+  ]);
+  const [Role] = await db.query("SELECT id FROM role WHERE title = ?", [
+    input.roleName,
+  ]);
+  const role_id = Role[0].id;
+
+  const [Manager] = await db.query(
+    "SELECT id FROM employee WHERE first_name = ?",
+    [input.managerName]
+  );
+  const manager_id = Manager[0].id;
+
+  const [result] = await db.query(
+    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)",
+    [input.employeeFirstName, input.employeeLastName, role_id, manager_id]
+  );
+
+  console.log("Role added successfully", result);
+  startpage();
+}
+
+startpage();
